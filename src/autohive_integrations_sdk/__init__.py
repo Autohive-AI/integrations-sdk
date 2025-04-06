726f49d872e8e87564198a68bb98bbd887b91866
Kai Koenig <kai@ventego-creative.co.nz>
2025-04-07 10:16:14 +1200

    Fixing an issue on lambdas

--- src/autohive_integrations_sdk/__init__.py
+++ src/autohive_integrations_sdk/__init__.py
@@ -1,4 +1,4 @@
 # Re-export classes from integration module
 from autohive_integrations_sdk.integration import (
-    Integration, ExecutionContext, ActionHandler, PollingTriggerHandler
+    Integration, ExecutionContext, ActionHandler, PollingTriggerHandler, ValidationError
 )

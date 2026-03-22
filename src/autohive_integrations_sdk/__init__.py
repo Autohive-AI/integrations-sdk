e8cde46288cda59dd303169369120845f7d2c27d
NinosMan <ninos.awas@gmail.com>
2026-03-23 12:50:35 +1300

    revert api-fetch sample to master, remove HTTPError export
    The ActionError/HTTPError changes don't belong in this sample.
    HTTPError can be exported when it's actually needed.

--- src/autohive_integrations_sdk/__init__.py
+++ src/autohive_integrations_sdk/__init__.py
@@ -4,6 +4,5 @@ __version__ = "1.0.2"
 # Re-export classes from integration module
 from autohive_integrations_sdk.integration import (
     Integration, ExecutionContext, ActionHandler, PollingTriggerHandler, ConnectedAccountHandler,
-    ConnectedAccountInfo, ValidationError, ActionResult, ActionError, IntegrationResult, ResultType,
-    HTTPError
+    ConnectedAccountInfo, ValidationError, ActionResult, ActionError, IntegrationResult, ResultType
 )

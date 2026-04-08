4569a9af4d90a81ca4b5ddd163c12af5c9f07222
NinosMan <ninos.awas@gmail.com>
2026-04-09 08:17:42 +1200

    feat: export HTTPError and RateLimitError from package

--- src/autohive_integrations_sdk/__init__.py
+++ src/autohive_integrations_sdk/__init__.py
@@ -4,5 +4,6 @@ __version__ = "1.1.0"
 # Re-export classes from integration module
 from autohive_integrations_sdk.integration import (
     Integration, ExecutionContext, ActionHandler, PollingTriggerHandler, ConnectedAccountHandler,
-    ConnectedAccountInfo, ValidationError, ActionResult, ActionError, IntegrationResult, ResultType
+    ConnectedAccountInfo, ValidationError, HTTPError, RateLimitError,
+    ActionResult, ActionError, IntegrationResult, ResultType
 )
